67bf3a7b3280af3447665e94b84445e9d846801c
吴沛聪 <1097727091@qq.com>
2021-06-02 17:04:26 +0800

    2021C P4 3

--- 2021C/P4/Template/main.cpp
+++ 2021C/P4/Template/main.cpp
@@ -14,32 +14,244 @@
 
 using namespace std;
 
+struct Wint :vector<int>
+{
+	Wint(int n = 0)
+	{
+		push_back(n);
+		check();
+	}
+	Wint& check()
+	{
+		while (!empty() && !back())pop_back();
+		if (empty())return *this;
+		for (int i = 1; i < size(); ++i)
+		{
+			(*this)[i] += (*this)[i - 1] / 10;
+			(*this)[i - 1] %= 10;
+		}
+		while (back() >= 10)
+		{
+			push_back(back() / 10);
+			(*this)[size() - 2] %= 10;
+		}
+		return *this;
+	}
+};
+
+istream& operator>>(istream& is, Wint& n)
+{
+	string s;
+	is >> s;
+	n.clear();
+	for (int i = s.size() - 1; i >= 0; --i)n.push_back(s[i] - '0');
+	return is;
+}
+ostream& operator<<(ostream& os, const Wint& n)
+{
+	if (n.empty())os << 0;
+	for (int i = n.size() - 1; i >= 0; --i)os << n[i];
+	return os;
+}
+
+
+bool operator!=(const Wint& a, const Wint& b)
+{
+	if (a.size() != b.size())return 1;
+	for (int i = a.size() - 1; i >= 0; --i)
+		if (a[i] != b[i])return 1;
+	return 0;
+}
+bool operator==(const Wint& a, const Wint& b)
+{
+	return !(a != b);
+}
+bool operator<(const Wint& a, const Wint& b)
+{
+	if (a.size() != b.size())return a.size() < b.size();
+	for (int i = a.size() - 1; i >= 0; --i)
+		if (a[i] != b[i])return a[i] < b[i];
+	return 0;
+}
+bool operator>(const Wint& a, const Wint& b)
+{
+	return b < a;
+}
+bool operator<=(const Wint& a, const Wint& b)
+{
+	return !(a > b);
+}
+bool operator>=(const Wint& a, const Wint& b)
+{
+	return !(a < b);
+}
+
+Wint& operator+=(Wint& a, const Wint& b)
+{
+	if (a.size() < b.size())a.resize(b.size());
+	for (int i = 0; i != b.size(); ++i)a[i] += b[i];
+	return a.check();
+}
+Wint operator+(Wint a, const Wint& b)
+{
+	return a += b;
+}
+
+Wint& operator-=(Wint& a, Wint b)
+{
+	if (a < b)swap(a, b);
+	for (int i = 0; i != b.size(); a[i] -= b[i], ++i)
+		if (a[i] < b[i])
+		{
+			int j = i + 1;
+			while (!a[j])++j;
+			while (j > i)
+			{
+				--a[j];
+				a[--j] += 10;
+			}
+		}
+	return a.check();
+}
+Wint operator-(Wint a, const Wint& b)
+{
+	return a -= b;
+}
+
+Wint operator*(const Wint& a, const Wint& b)
+{
+	Wint n;
+	n.assign(a.size() + b.size() - 1, 0);
+	for (int i = 0; i != a.size(); ++i)
+		for (int j = 0; j != b.size(); ++j)
+			n[i + j] += a[i] * b[j];
+	return n.check();
+}
+Wint& operator*=(Wint& a, const Wint& b)
+{
+	return a = a * b;
+}
+
+Wint divmod(Wint& a, const Wint& b)
+{
+	Wint ans;
+	for (int t = a.size() - b.size(); a >= b; --t)
+	{
+		Wint d;
+		d.assign(t + 1, 0);
+		d.back() = 1;
+		Wint c = b * d;
+		while (a >= c)
+		{
+			a -= c;
+			ans += d;
+		}
+	}
+	return ans;
+}
+Wint operator/(Wint a, const Wint& b)
+{
+	return divmod(a, b);
+}
+Wint& operator/=(Wint& a, const Wint& b)
+{
+	return a = a / b;
+}
+Wint& operator%=(Wint& a, const Wint& b)
+{
+	divmod(a, b);
+	return a;
+}
+Wint operator%(Wint a, const Wint& b)
+{
+	return a %= b;
+}
+
+Wint pow(const Wint& n, const Wint& k)
+{
+	if (k.empty())return 1;
+	if (k == 2)return n * n;
+	if (k.back() % 2)return n * pow(n, k - 1);
+	return pow(pow(n, k / 2), 2);
+}
+
+
 typedef enum op_type {
-	NUM = 0, ADD = 1, MULT = 2, SPEC = 3, SERIALIZED = 4
+	NUM = 0, ADD = 1, MULT = 2, SPEC = 3
 }op_type;
+
+class sp{
+public:
+	Wint a;
+	Wint b;
+
+	sp(Wint _a, Wint _b) :a(_a), b(_b) {
+
+	}
+
+	bool operator>(const sp& rhs) const{
+		if (a > rhs.a) {
+			return true;
+		}
+		else if (a == rhs.a) {
+			if (b > rhs.b) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	bool operator<(const sp& rhs) const{
+		if (a < rhs.a) {
+			return true;
+		}
+		else if (a == rhs.a) {
+			if (b < rhs.b) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	bool operator==(const sp& rhs) const{
+		if (a == rhs.a && b == rhs.b) {
+			return true;
+		}
+		return false;
+	}
+};
+
+map<sp, Wint> term;
+
+Wint getValue(Wint a,Wint b) {
+	Wint res;
+	auto i = term.find(sp(a,b));
+	if (term.end() == i) {
+		res = rand();
+		auto p = make_pair(sp(a,b),res);
+		term.insert(p);
+	}
+	else {
+		res = i->second;
+	}
+	return res;
+}
 
 class item{
 public:
 	op_type t;
-	union {
-		int v;
-		struct {
-			vector<item*>* s;
-			vector<int> *coef;
-		};
-	};
+	Wint v;
 	item* l;
 	item* r;
-	item* p;
 
-	item(int _num) :
+	item(Wint _num) :
 		t(NUM), v(_num) {
-		l = r = p = NULL;
+		l = r  = NULL;
 	}
 
 	item(enum op_type _t): 
 	t(_t),v(0){
-		l = r = p = NULL;
+		l = r  = NULL;
 	}
 
 	item(const item& rhs):
@@ -57,95 +269,25 @@ public:
 			r = NULL;
 		}
 	}
-	
 	~item() {
-		if (SERIALIZED == t) {
-			delete s;
-		}
-	}
-	bool operator ==(const item &rhs) {
-		if (t != rhs.t) {
-			return false;
-
-		}
-
-		if(t == NUM && v != rhs.v) {
-			return false;
-		}
-
-		if (l != rhs.l) {
-			return false;
-		}
-		else if (l != NULL) {
-			if (!((*l) == (*rhs.l))) {
-				return false;
-			}
-		}
-
-		if (r != rhs.r) {
-			return false;
-		}
-		else if (r != NULL) {
-			if (!((*r) == (*rhs.r))) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-
-	bool operator >(const item &rhs) {
-		if (t > rhs.t) {
-			return true;
-		}
-
-		if (v > rhs.v) {
-			return true;
-		}
-
 		if (l != NULL) {
-			if (rhs.l != NULL) {
-				if ((*l) > (*rhs.l)) {
-					return true;
-				}
-				else if((*rhs.l) > (*l)){
-					return false;
-				}
-			}
-			else {
-				return true;
-			}
+			delete l;
 		}
-		else if (rhs.l != NULL) {
-			return false;
-		}
-
 		if (r != NULL) {
-			if (rhs.r != NULL) {
-				if ((*r) > (*rhs.r)) {
-					return true;
-				}
-				else if((*rhs.r) > (*r)){
-					return false;
-				}
-			}
-			else {
-				return true;
-			}
-		}
-		else if (rhs.r != NULL) {
-			return false;
+			delete r;
 		}
 	}
 };
 
 item* parse_exp() {
 	item* p = NULL, * l = NULL, * r = NULL;
+	bool bra = false;
 	char c = cin.peek();
 	if (c == ' ' || c == '\n') {
-		cin >> c;
+		cin.ignore(1);
 		c = cin.peek();
 	}
+
 	if (c == '(') {
 		cin >> c;
 		l = parse_exp();
@@ -155,7 +297,10 @@ item* parse_exp() {
 		cin >> lval;
 		l = new item(lval);
 	}
-
+	c = cin.peek();
+	if (c != '+' && c != '*' && c != '#') {
+		return l;
+	}
 	cin >> c;
 	switch (c) {
 	case '+':
@@ -175,15 +320,14 @@ item* parse_exp() {
 		r = parse_exp();
 	}
 	else {
-		int rval;
+		Wint rval;
 		cin >> rval;
 		r = new item(rval);
 	}
+	cin >> c;
 	if (p != NULL) {
 		p->l = l;
 		p->r = r;
-		l->p = p;
-		r->p = p;
 	}
 	return p;
 }
@@ -197,176 +341,21 @@ item* reduce(item *e) {
 	case SPEC:
 		e->l = reduce(e->l);
 		e->r = reduce(e->r);
-		//e->v = match_spec(*e);
+		e->v = getValue(e->l->v, e->r->v);
 		return e;
 		break;
 	case ADD: 
 		e->l = reduce(e->l);
 		e->r = reduce(e->r);
-		if (e->l->t == NUM && e->r->t == NUM) {
-			e->v = e->l->v + e->r->v;
-			delete e->l;
-			delete e->r;
-			e->t = NUM;
-			e->l = NULL;
-			e->r = NULL;
-		}
-		else if (e->l->t == NUM && e->l->v == 0) {
-			item* r = e->r;
-			r->p = e->p;
-			delete e->l;
-			delete e;
-			return r;
-		}
-		else if (e->r->t == NUM && e->r->v == 0) {
-			item* l = e->l;
-			l->p = e->p;
-			delete e->r;
-			delete e;
-			return l;
-		}
+		e->v = e->l->v + e->r->v;
 		break;
 	case MULT:
 		e->l = reduce(e->l);
 		e->r = reduce(e->r);
-		if (e->l->t == NUM && e->r->t == NUM) {
-			e->v = e->l->v * e->r->v;
-			delete e->l;
-			delete e->r;
-			e->l = NULL;
-			e->r = NULL;
-			return e;
-		}
-		else if (e->l->t == NUM) {
-			if (e->l->v == 1) {
-				item* r = e->r;
-				r->p = e->p;
-				delete e->l;
-				delete e;
-				return r;
-			}
-			else if (e->l->v == 0) {
-				item* r = new item(0);
-				r->p = e->p;
-				delete e->r;
-				delete e->l;
-				delete e;
-				return r;
-			}
-		}
-		else if (e->r->t == NUM) {
-			if (e->r->v == 1) {
-				item* l = e->l;
-				l->p = e->p;
-				delete e->r;
-				delete e;
-				return l;
-			}
-			else if (e->r->v == 0) {
-				item* l = new item(0);
-				l->p = e->p;
-				delete e->r;
-				delete e->l;
-				delete e;
-				return l;
-			}
-		}
-		if (e->l->t == ADD) {
-			item* p = new item(ADD);
-			p->p = e->p;
-			p->l = new item(MULT);
-			p->r = new item(MULT);
-			p->l->p = p;
-			p->r->p = p;
-			p->l->l = e->l->l;
-			p->l->r = e->r;
-			p->r->l = e->l->r;
-			p->r->r = new item(*e->r);
-
-			p->l->l->p = p->l;
-			p->l->r->p = p->l;
-			p->r->l->p = p->r;
-			p->r->r->p = p->r;
-			delete e->l;
-			delete e;
-			reduce(p);
-			return p;
-		}
-		if (e->r->t == ADD) {
-			item* p = new item(ADD);
-			p->p = e->p;
-			p->l = new item(MULT);
-			p->r = new item(MULT);
-			p->l->p = p;
-			p->r->p = p;
-			p->l->r = e->r->l;
-			p->l->l = e->l;
-			p->r->r = e->r->r;
-			p->r->l = new item(*e->l);
-
-			p->l->l->p = p->l;
-			p->l->r->p = p->l;
-			p->r->l->p = p->r;
-			p->r->r->p = p->r;
-			delete e->r;
-			delete e;
-			reduce(p);
-			return p;
-		}
+		e->v = e->l->v * e->r->v;
 		break;
 	}
-	if ((*e->l) > (*e->r)) {
-		item* temp = e->l;
-		e->l = e->r;
-		e->r = temp;
-	}
 	return e;
-}
-
-//combine terms
-item *serialize(item *p) {
-	switch (p->t) {
-		case NUM:
-			p->t = SERIALIZED;
-			item* tmp = new item(1);
-			p->s = new vector<item*>(1);
-			(*p->s)[0] = tmp;
-			(*p->coef)[0] = p->v;
-			break;
-		case ADD:
-			p->t = SERIALIZED;
-			p->s = new vector<item*>;
-			if (p->l->t == SERIALIZED && p->r->t == SERIALIZED) {
-				int i = 0;
-				int j = 0;
-				int k = 0;
-				vector<int>* coefa = p->l->coef, *coefb = p->r->coef;
-				vector<item*>* sa = p->l->s, * sb = p->r->s;
-				p->s->resize(sa->size() + sb->size());
-				p->coef->resize(coefa->size() + coefb->size());
-				while (i < sa->size() &&
-					j < sb->size()) {
-					if (*(*sa)[i] > *(*sb)[j]) {
-						(*p->s)[k] = (*sb)[j];
-						(*p->coef)[k] = (*coefb)[j];
-						j++;
-					}
-					else if (*(*sa)[i] == *(*sb)[j]) {
-						(*p->s)[k] = (*sa)[i];
-						(*p->coef)[k] = (*coefa)[i] + (*coefb)[j];
-						i++;
-						j++;
-					}
-					else {
-						(*p->s)[k] = (*sa)[i];
-						(*p->coef)[k] = (*coefa)[i];
-						i++;
-					}
-					k++;
-				}
-				
-			}
-	}
 }
 
 int main()
@@ -378,11 +367,16 @@ int main()
 	for (int case_count = 1; case_count <= case_number; case_count++) {
 		int n;
 		cin >> n;
-		vector<vector<item>> exp(n);
+		vector<Wint> exp(n);
 		for (int i = 0; i < n; i++) {
 			item *e = parse_exp();
 			e = reduce(e);
+			exp[i] = e->v;
 		}
-		cout << "Case #" << case_count << ": " << endl;
+		cout << "Case #" << case_count << ": ";
+		for (int i = 0; i < n; i++) {
+			cout << exp[i] << " " ;
+		}
+		cout << endl;
 	}
 }
